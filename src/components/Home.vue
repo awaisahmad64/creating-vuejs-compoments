45715ce03b63cb27e8a14db362cf5c4a9c5b3051
webxsolutions <awais6412l@gmail.com>
2021-06-08 16:10:55 +0500

    add simple compomnent

---
+++ src/components/Home.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h1>This is my First Component</h1>
+        <h2>{{data}}<br>{{msg}}</h2>
+        <button v-on:mouseover="hello('parameter pass to call fun')">call function</button>
+    </div>
+</template>
+<script>
+export default {
+    name: 'Home',
+    props:{
+        data: String,
+        msg: String
+    },methods:{
+        hello(data){
+            alert(data);
+        }
+    }
+}
+</script>
